db5f5a5dc8e9eeee1f213301fecc2291c577f902
LieStage <118051769+LieStage@users.noreply.github.com>
2022-12-03 11:04:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot! of TRUMBOTS IF YOU WANT THIS TYPE OF BOT CONTACT @FLIGHER"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
